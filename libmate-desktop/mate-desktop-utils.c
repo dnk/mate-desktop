3ca9e78440f39b206e6eab78b00d186322e0eb1c
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2014-02-18 13:09:49 +0400

    append variable declaration
    which lost during merge

--- libmate-desktop/mate-desktop-utils.c
+++ libmate-desktop/mate-desktop-utils.c
@@ -190,6 +190,7 @@ mate_gdk_spawn_command_line_on_screen (GdkScreen *screen, const gchar *command, 
 {
 	GAppInfo *appinfo = NULL;
 	GdkAppLaunchContext *context = NULL;
+	gboolean res = FALSE;
 
 	appinfo = g_app_info_create_from_commandline (command, NULL, G_APP_INFO_CREATE_NONE, error);
 
